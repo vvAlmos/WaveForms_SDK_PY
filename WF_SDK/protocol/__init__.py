2b06e50d8a85d72a108582b54ce537cdce0de402
Álmos Veres-Vitályos <almos.veres-vitalyos@digilent.ro>
2022-01-20 15:37:51 +0200

    update initializers

--- WF_SDK/protocol/__init__.py
+++ WF_SDK/protocol/__init__.py
@@ -2,12 +2,6 @@
 This module controls the protocol instrument
 """
 
-import i2c as I2C
-import spi as SPI
-import uart as UART
-
-class protocol:
-    i2c = I2C
-    spi = SPI
-    uart = UART
-
+"""from WF_SDK.protocol import i2c
+from WF_SDK.protocol import spi
+from WF_SDK.protocol import uart"""
